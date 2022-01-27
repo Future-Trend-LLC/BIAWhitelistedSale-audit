01d5a9da1b17ba73a715591d253668de8d886029
Arunatr85 <98005140+Arunatr85@users.noreply.github.com>
2022-01-27 23:01:32 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,7 +11,7 @@ wallet -> 0xF2Dec16aD61B58c5b914419548747c72eF451Dc5 (for Test)
 Token Address -> 0xbe050b2e55a4d3002d215bc90528c7711a3fd7e0 (for Test)
 CAP -> 46893317702227400000000  (BNB value of 200000000 BIA Token as given rate)   
 OpeningTime -> February 1, 2022 12:00:01 AM - 1643673601 (New value will be assigned during the deployment)
-ClosingTime -> May 1, 2022 12:00:01 AM - 1651363201 (from new value,Three months)
+ClosingTime -> May 1, 2022 12:00:01 AM - 1651363201 (from new value, Three months)
 ReleaseTime1 -> July 1, 2022 12:00:01 AM - 1656633601
 ReleaseTime2 ->January 1, 2023 12:00:01 AM - 1672531201
 ReleaseTime3 ->January 1, 2024 12:00:01 AM - 1704067201
