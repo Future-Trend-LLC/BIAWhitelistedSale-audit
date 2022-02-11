f88d2dc3cf1dec5769f99d6b52734564a38519a1
Arunatr85 <98005140+Arunatr85@users.noreply.github.com>
2022-02-11 10:09:47 +0530

    Update BIAWhitelistedSale.sol

--- contracts/BIAWhitelistedSale.sol
+++ contracts/BIAWhitelistedSale.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/TokenTimelock.sol";
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
